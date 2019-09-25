92ef11b4f788b2a8ee10624fea74939e8eab56cd
yhorie <yuk.horie@gmail.com>
2019-09-25 09:58:40 +0900

    Finish 'Importing Workbox'

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,1 +1,7 @@
-console.log('service-worker.js');
+self.importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+
+if (self.workbox) {
+  console.log(`Yay! workbox is loaded`);
+} else {
+  console.log(`Boo! Workbox didn't load`);
+}
